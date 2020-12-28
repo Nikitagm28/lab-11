edcdaef680a34e33e9eeec0498a8254add015848
Nikita <nikita-kuznetsov-2018@list.ru>
2020-12-28 20:57:29 +0300

    Initial Commit

---
+++ 11/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  11
+//
+//  Created by Никита Кузнецов on 28.12.2020.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
